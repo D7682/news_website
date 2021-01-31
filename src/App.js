b686e2c49eefb057631556310b2f0910fd1f0b73
Daniel Gomez <danielgomez7682@gmail.com>
2021-01-30 18:13:36 -0600

    fixes

--- src/App.js
+++ src/App.js
@@ -2,32 +2,27 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./component/Card";
 import Navbar from "./component/Navbar";
-import Guardian from "guardian-js";
 
-const guardian = new Guardian("00ad7cf7-79de-494d-817a-9b425a0c5582", false);
+const API_KEY = "00ad7cf7-79de-494d-817a-9b425a0c5582";
 
 function App() {
-  const [author, setAuthor] = useState({});
+  const [author, setAuthor] = useState();
 
-  // useEffect?
   useEffect(() => {
-    guardian.editions
-      .search("us")
-      .then((x) => x)
-      .then((x) => console.log(x))
-      .catch((err) => console.log(err));
-  });
+    fetch(`https://content.guardianapis.com/search?q=debate&tag=politics/politics&from-date=2014-01-01&show-fields=thumbnail&api-key=${API_KEY}`)
+      .then((x) => x.json())
+      .then((x) => x.response)
+      .then((x) => setAuthor(x.results.map((x, index) => <Card title={x.webTitle} images={x.fields.thumbnail} link={x.webUrl} key={index} />)))
+      .catch((err) => console.log(err))
+  }, [])
 
   return (
     <div className="container-fluid">
       <Navbar></Navbar>
       <div className="row">
         <div className="col-md-8">
-          <div className="row">
-            <Card title={author}></Card>
-            {/* <Card></Card>
-                <Card></Card>
-                <Card></Card> */}
+          <div className="row justify-items-center">
+            {author}
           </div>
         </div>
         <div className="col-md-4"></div>
--- src/component/Card.js
+++ src/component/Card.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const Card = ({author}) => {
+const Card = ({title, link, images}) => {
     return (
         <div className="col-md-4">
-            <div class="card" style={{width: "18rem"}}>
-                <img src="/" class="card-img-top" alt="..."/>
-                <div class="card-body">
-                    <h5 class="card-title">{author}</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="/" class="btn btn-primary">Go somewhere</a>
+            <div className="card" style={{width: "18rem"}}>
+                <img src={images} className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href={link} target="_blank" rel="noreferrer" className="btn btn-primary">Article</a>
                 </div>
             </div>
         </div>
